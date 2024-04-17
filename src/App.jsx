112215a562e523cf064dc47f4fe6dc27cae48a54
Rajkumar Kalita <rajkumarriyan001@gmail.com>
2024-04-17 18:40:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+    return (
+        <>
+            <Button varient="secondary" size="lg">
+                CLick Me
+            </Button>
+        </>
+    );
+}
+
+export default App;
